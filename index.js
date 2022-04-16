71f1f3c57e99cd8268cc3d79e1ac1f9c6d4d4308
Miguel <miguel-s-g@hotmial.com>
2022-04-16 19:03:56 +0200

    push inicial 3
    push inicial 3

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const { Pool, pool } = require('./config')
 const { request } = require('express')
 const { response } = require('express')
 
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
+const { body, check } = require('express-validator')
+
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
 const getPeople = (request, response) => {
 	pool.query('SELECT * FROM People', (error, results) => {
-		if (error){
+		if (error) {
 			throw error
 		}
 		response.status(200).json(results.rows)
@@ -23,7 +28,7 @@ const getPeople = (request, response) => {
 }
 
 const addPerson = (request, response) => {
-	const {personID, firstName, lastName, age, gender, isUser, emailAddress, phoneNumber} = request.body
+	const { personID, firstName, lastName, age, gender, isUser, emailAddress, phoneNumber } = request.body
 	pool.query(
 		'INSERT INTO People (PersonID, FirstName, LastName, Age, Gender, IsUser, EmailAddress, PhoneNumber) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
 		[personID, firstName, lastName, age, gender, isUser, emailAddress, phoneNumber],
@@ -31,18 +36,36 @@ const addPerson = (request, response) => {
 			if (error) {
 				throw error
 			}
-			response.status(201).json({ status: 'success', message: 'PersonAdded.'})
+			response.status(201).json({ status: 'success', message: 'PersonAdded.' })
 		}
 	)
 }
 
 app
-	.route('/people')
+	.route('/')
+	.get((req, res) => {
+		res.send('hello world')
+	})
+
+	/*
+app.route('/people')
 	.get(getPeople)
-	.post(addPerson)
-
-// start server
-
+	.post(
+	(request, response) => {
+		const { personID, firstName, lastName, age, gender, isUser, emailAddress, phoneNumber } = request.body
+		pool.query(
+			'INSERT INTO People (PersonID, FirstName, LastName, Age, Gender, IsUser, EmailAddress, PhoneNumber) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+			[personID, firstName, lastName, age, gender, isUser, emailAddress, phoneNumber],
+			(error) => {
+				if (error) {
+					throw error
+				}
+				response.status(201).json({ status: 'success', message: 'PersonAdded.' })
+			}
+		)
+	}
+)
+*/
 const port = process.env.port || 3000
 app.listen(port, () => {
 	console.log(`Running on port ${port}`)
