66f4b8050c698c77f2e4028e78788fd33cc8626e
Miguel <miguel-s-g@hotmial.com>
2022-06-11 23:05:48 +0200

    unsubscribeUser

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post('/people', db.createPerson)
 
 app.get('/people/:personID', db.getPersonByID) //OK
 app.put('/people/:personID', db.updatePersonalData)
-app.put('/people/:personID', db.unsubscribe_subscribe)
+app.put('/people/:personID/unsub', db.unsubscribe_subscribe)
 
 app.get('/people/responsibles', db.getAllResponsibles) //CHECK
 
